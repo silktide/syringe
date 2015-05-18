039aca01672d686a182b1ce854d1c109b320e3b9
Doug <dougnelson@silktide.com>
2015-05-18 11:50:03 +0100

    added yaml support

---
+++ src/Loader/YamlLoader.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doug
+ * Date: 18/05/15
+ * Time: 11:42
+ */
+
+namespace Silktide\Syringe\Loader;
+
+
+use Silktide\Syringe\Exception\LoaderException;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
+
+class YamlLoader implements LoaderInterface {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return "YAML Loader";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($file)
+    {
+        return (in_array(pathinfo($file, PATHINFO_EXTENSION), ["yml", "yaml"]));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws \Silktide\Syringe\Exception\LoaderException
+     */
+    public function loadFile($file)
+    {
+        $parser = new Parser();
+        try {
+            $data = $parser->parse(file_get_contents('/path/to/file.yml'));
+        } catch (ParseException $e) {
+            printf("Unable to parse the YAML string: %s", $e->getMessage());
+            throw new LoaderException(sprintf("Could not load the YAML file '%s': %s", $file, $e->getMessage()));
+        }
+        return $data;
+    }
+}
