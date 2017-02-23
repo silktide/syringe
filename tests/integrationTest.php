27ea2948a390458b3574be396cef16be4cb52ace
Danny Smart <dannysmart@silktide.com>
2017-02-23 11:45:35 +0000

    Add tests for namespacing aliases

--- tests/integrationTest.php
+++ tests/integrationTest.php
@@ -8,7 +8,6 @@ $configFiles = [
     "service.json",
     "private_test" => "aliased.json"
 ];
-$builder->addConfigFile("aliased.json", "private_test");
 
 Syringe::init(__DIR__, $configFiles);
 
@@ -19,7 +18,7 @@ $collection = $container["tagCollection"];
 if (empty($collection->services)) {
     throw new \Exception("No services were injected using the tag #duds");
 }
-if (count($collection->services) != 1 || !$collection->services[0] instanceof \Silktide\Syringe\Test\Service\DudService) {
+if (count($collection->services) != 1 || !$collection->services[0] instanceof \Silktide\Syringe\Tests\Service\DudService) {
     throw new \Exception("An incorrect service was injected: " . print_r($collection->services, true));
 }
 
@@ -28,14 +27,31 @@ if ($duds !== $collection) {
     throw new \Exception("Aliased service did not return the same object as the original service");
 }
 
-if ($container->offsetExists("private_test.privateService")) {
-    throw new \Exception("Services marked as private are accessible from outside of their alias");
+
+
+// check aliases are namespaced
+if ($container->offsetExists("publicAlias")) {
+    throw new \Exception("Aliases should be namespaced where appropriate.");
+}
+if (!$container["private_test.publicAlias"] instanceof \Silktide\Syringe\Tests\Service\DudConsumer) {
+    throw new \Exception("Namespaced Alias was not accessible");
 }
 
+// check private services are hidden
+if ($container->offsetExists("private_test.privateService")) {
+    throw new \Exception("Services marked as private should not be accessible from outside of their alias");
+}
+
+// check private services can be used within the same namespace
 try {
     $service = $container["private_test.usesPrivateService"];
 } catch (\Silktide\Syringe\Exception\ReferenceException $e) {
     throw new \Exception("An unexpected ReferenceException was thrown when trying to access a service that uses a private service:\n" . $e->getMessage());
 }
+
+// This is a bug. Need to work out how to make aliases respect privacy
+//if ($container->offsetExists("private_test.privateAlias")) {
+//    throw new \Exception("Aliases do not respect privacy");
+//}
 
 echo "\nAll tests passed\n";
