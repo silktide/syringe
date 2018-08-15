8ba881d09c2d85bb097ff5b2e2fa1b4f0299c0d3
Dolondro <dougnelson@silktide.com>
2018-08-15 15:45:20 +0100

    updated yaml loader to support pre-3.4 versions of symfony yaml

--- src/Loader/YamlLoader.php
+++ src/Loader/YamlLoader.php
@@ -10,14 +10,15 @@ use Symfony\Component\Yaml\Yaml;
 class YamlLoader implements LoaderInterface
 {
     protected $hasSymfony;
+    protected $hasExtension;
     protected $forceSymfony;
 
     public function __construct(bool $useSymfony = false)
     {
-        $hasSymfony = class_exists('Symfony\Component\Yaml\Yaml');
-        $hasExtension = function_exists("yaml_parse_file");
+        $this->hasSymfony = class_exists('Symfony\Component\Yaml\Yaml') && method_exists('Symfony\Component\Yaml\Yaml', 'parse');
+        $this->hasExtension = function_exists("yaml_parse");
 
-        if (!$hasSymfony && !$hasExtension) {
+        if (!$this->hasSymfony && !$this->hasExtension) {
             throw new \Exception('Either Symfony\Yaml or the Yaml PHP extension is required to use this loader');
         }
         $this->forceSymfony = $useSymfony;
@@ -49,10 +50,10 @@ class YamlLoader implements LoaderInterface
             throw new LoaderException("Requested YAML file '{$file}' doesn't exist");
         }
 
-        if (!$this->forceSymfony && function_exists("yaml_parse_file")) {
+        if (!$this->forceSymfony && $this->hasExtension) {
             $data = yaml_parse_file($file);
         } else {
-            $data = Yaml::parseFile($file);
+            $data = Yaml::parse(file_get_contents($file));
         }
 
         if (!is_array($data)) {
