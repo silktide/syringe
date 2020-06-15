c978aadef68cebecc8bf38798f3a9839ebe3c8ce
Doug Nelson <dougnelson@silktide.com>
2020-06-15 15:47:06 +0100

    add test for laziness

--- tests/integration/Lazy/LazyTest.php
+++ tests/integration/Lazy/LazyTest.php
@@ -10,6 +10,11 @@ use Silktide\Syringe\Syringe;
 
 class LazyTest extends TestCase
 {
+    public function setUp() : void
+    {
+        LazyClass::$loaded = false;
+    }
+
     public function testLazyLoading()
     {
         $container = Syringe::build([
@@ -25,5 +30,21 @@ class LazyTest extends TestCase
         $this->assertFalse(LazyClass::$loaded);
         $myService->getTrue();
         $this->assertTrue(LazyClass::$loaded);
+    }
+
+    public function testNonLazyLoading()
+    {
+        $container = Syringe::build([
+            "paths" => [__DIR__],
+            "files" => ["file2.yml"]
+        ]);
+
+        $this->assertFalse(LazyClass::$loaded);
+        /**
+         * @var LazyClass $myService
+         */
+        $myService = $container["my_service"];
+        $this->assertTrue(LazyClass::$loaded);
+        $this->assertInstanceOf(LazyClass::class, $myService);
     }
 }
